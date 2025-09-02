271ed48bfea24ba680698d6fd49d2bc713cddf72
Rehman Ahmed <rehmanahmeddxb@gmail.com>
2025-09-02 07:38:11 +0000

    DATE SHOULD BE IN SINGLE LINE LINEAR

--- src/components/date-range-picker.tsx
+++ src/components/date-range-picker.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -33,7 +34,7 @@ export function DateRangePicker({
             id="date"
             variant={"outline"}
             className={cn(
-              "w-full sm:w-[300px] justify-start text-left font-normal",
+              "w-[300px] justify-start text-left font-normal",
               !date && "text-muted-foreground"
             )}
           >
